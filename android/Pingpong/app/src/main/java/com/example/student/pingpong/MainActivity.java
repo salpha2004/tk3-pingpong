6e913c7e75af7b22d5238d9aad2e01908bdd572b
masaud <sophismay@gmail.com>
2016-04-21 15:22:04 +0200

    umundo integration: library import

--- android/Pingpong/app/src/main/java/com/example/student/pingpong/MainActivity.java
+++ android/Pingpong/app/src/main/java/com/example/student/pingpong/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.student.pingpong;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.umundo.core.Discovery;
+import org.umundo.core.Discovery.DiscoveryType;
+
 
 public class MainActivity extends AppCompatActivity {
 
